966818a9b4bc9b83dbc5f337d650ebca3755ecea
Eugene Godun <eugene.godun@gmail.com>
2014-10-08 16:20:21 +0300

    index adaptive

--- js/common.js
+++ js/common.js
@@ -17,7 +17,7 @@ head.ready(function() {
 		dots: true,
 		arrows: false
 	});
-	$('.js-slick-clients').slick({
+	$('.js-slider-clients').slick({
 		dots: false,
 		infinite: true,
 		speed: 300,
@@ -29,23 +29,93 @@ head.ready(function() {
 		      breakpoint: 980,
 		      settings: {
 		        slidesToShow: 3,
-		        slidesToScroll: 1
+		        slidesToScroll: 1,
+		        arrows: false
 		      }
 		    },
 		    {
 		      breakpoint: 768,
 		      settings: {
 		        slidesToShow: 2,
-		        slidesToScroll: 1
+		        slidesToScroll: 1,
+		        arrows: false
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        centerMode: true,
+        		centerPadding: '60px',
+       			slidesToShow: 1,
+		        arrows: false
 		      }
 		    }
-		  ]
+		]
+	});
+
+	$('.js-slider-banners').slick({
+		dots: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		arrows: false,
+		responsive: [
+		    {
+		      breakpoint: 768,
+		      settings: {
+		      	centerMode: true,
+        		centerPadding: '45px',
+       			slidesToShow: 1,
+		        arrows: false
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		      	centerMode: true,
+        		centerPadding: '45px',
+       			slidesToShow: 1,
+		        arrows: false
+		      }
+		    }
+		]
+	});
+	$('.js-slider-news').slick({
+		dots: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		arrows: false,
+		adaptiveHeight: true,
+		responsive: [
+		    {
+		      breakpoint: 780,
+		      settings: {
+       			slidesToShow: 2,
+       			slidesToScroll: 1,
+		        arrows: false,
+		        adaptiveHeight: true
+		      }
+		    },
+		    {
+		      breakpoint: 640,
+		      settings: {
+       			slidesToShow: 1,
+       			slidesToScroll: 1,
+		        arrows: false,
+		        dots: true,
+		        adaptiveHeight: true
+		      }
+		    }
+		]
 	});
 
 	$(".js-client-prev").on("click",function(){
-		$('.js-slick-clients').slickPrev();
+		$('.js-slider-clients').slickPrev();
 	});
 	$(".js-client-next").on("click",function(){
-		$('.js-slick-clients').slickNext();
+		$('.js-slider-clients').slickNext();
 	});
 });
