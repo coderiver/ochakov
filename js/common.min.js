ebf83c94ed90a2691b368edcf86a52b2a47f96ef
Eugene Godun <eugene.godun@gmail.com>
2014-10-10 12:50:06 +0300

    <picture> slider

--- js/common.min.js
+++ js/common.min.js
@@ -3,6 +3,8 @@ head.ready(function() {
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
 	// });
+	
+
 
 	$(".js-toggle-key").on("click", function(){
 		var el = $(this).attr("data-toggle");
